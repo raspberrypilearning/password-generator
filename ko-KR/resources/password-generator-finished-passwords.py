9fb9c7311d7f3d7ec5ac8bf7e43365a6f0a4d6f4
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-08-25 14:12:05 +0100

    New translations password-generator-finished-passwords.py (Korean)

--- ko-KR/resources/password-generator-finished-passwords.py
+++ ko-KR/resources/password-generator-finished-passwords.py
@@ -3,19 +3,19 @@
 import random
 
 print('''
-Password Generator
+비밀번호 생성기
 ==================
 ''')
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
 
-number = input('number of passwords?')
+number = input('비밀번호의 개수를 입력하세요.')
 number = int(number)
 
-length = input('password length?')
+length = input('비밀번호의 길이를 입력하세요.')
 length = int(length)
 
-print('\nhere are your passwords:')
+print('\n' + '생성된 비밀번호입니다:')
 
 for pwd in range(number):
   password = ''
