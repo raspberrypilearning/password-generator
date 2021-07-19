6682740da4e89e95005ba9935e9a9c974eb107be
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2021-07-19 11:40:16 +0100

    New translations password-generator-finished-passwords.py (Polish)

--- pl-PL/resources/password-generator-finished-passwords.py
+++ pl-PL/resources/password-generator-finished-passwords.py
@@ -3,22 +3,22 @@
 import random
 
 print('''
-Password Generator
+Generator haseł
 ==================
 ''')
 
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
+znaki = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
 
-number = input('number of passwords?')
-number = int(number)
+ilosc = input('Podaj liczbę haseł: ')
+ilosc = int(ilosc)
 
-length = input('password length?')
-length = int(length)
+dlugosc = input('Podaj długość hasła: ')
+dlugosc = int(dlugosc)
 
-print('\nhere are your passwords:')
+print('\n' + 'Oto Twoje hasła:')
 
-for pwd in range(number):
-  password = ''
-  for c in range(length):
-    password += random.choice(chars)
-  print(password)
+for hsl in range(ilosc):
+  haslo = ''
+  for z in range(dlugosc):
+    haslo += random.choice(znaki)
+  print(haslo)
